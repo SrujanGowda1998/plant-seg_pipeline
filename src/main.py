c8dca368d986561e3c6c864218c6aef25f2679d5
SrujanGowda1998 <srujanprakashgowda3@gmail.com>
2026-03-18 15:42:01 +0100

    Commiting fix to filter

--- src/main.py
+++ src/main.py
@@ -10,12 +10,6 @@ from postprocessing import remove_bg_objects
 input_path = "data/tiff_files/ced3Gfp_worm1_01_488.tif"
 output_path = "outputs/results/"
 
-# # Create individual output folder
-# output_root = Path("outputs/results")
-# file_name = input_path.stem
-# output_path = output_root / file_name
-# output_path.mkdir(parents=True, exist_ok=True)
-
 # Load config file
 
 config = load_config(Path("config/config.yaml"))
@@ -49,41 +43,7 @@ export_image(boundary_map, output_path, "boundary_prediction")
 filter_name = config["preprocessing"]["filter_name"]
 
 filtered_boundary_map = filtered_boundary_map(boundary_map, rescaled_image, filter_name)
-export_image(region_of_interest, output_path, "boundary_prediction_filtered")
-
-############################################################################################
-
-#### correct boundary map
-# threshold_function='triangle' ### parameter from config file
-# from scipy import threshold_function as mythreshold ##or whatever python package has the image thresholding functions (look for otsu, yen, ...)
-# threshold_value = mythreshold(smootehned_image[:])  ### needs to run on the npy array of the image
-# filtered_image  = smoothened_image > threshold_value  ### maybe also rescaled? not sure what size the boundary_map is
-# boundary_map_corrected = boundary_map * filtered_image+
-
-
-# from skimage.filters import threshold_triangle, threshold_otsu, threshold_yen
-# import numpy as np
-# from plantseg.core.image import PlantSegImage
-
-# boundary_map_filtered = boundary_map
-
-# image_np = rescaled_image.get_data()
-# boundary_np = boundary_map.get_data()
-
-# Threshold
-# threshold_value = threshold_triangle(image_np)
-# # threshold_value = threshold_otsu(image_np)
-# threshold_value = threshold_yen(image_np)
-
-# # Mask
-# mask = image_np > threshold_value # Pixel wise comoarision
-# mask = mask.astype(np.float32) # Required for multiplication
-
-# boundary_filtered_np = boundary_np * mask
-# boundary_map_filtered = boundary_map_filtered.derive_new(boundary_filtered_np, name=f"{boundary_map_filtered.name}_filtered")
-# export_image(boundary_map_filtered, output_path, "boundary_prediction_filtered")
-
-############################################################################################
+export_image(filtered_boundary_map, output_path, "boundary_prediction_filtered")
 
 # Boundaries to Superpixels
 threshold = config["segmentation"]["threshold"]
